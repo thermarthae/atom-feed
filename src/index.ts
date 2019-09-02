70c27960bb72eecc5590c3d38e0587b09579afe7
thermarthae <thermarthae@protonmail.com>
2019-09-02 18:19:41 +0200

    Add support of additional text attributes

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
 import { js2xml } from 'xml-js';
 
+type TContentType = 'html' | 'xhtml' | 'text';
+type TContent = ITextConstruct | { type?: string; src: string; value: never };
+
+interface ITextConstruct {
+    type?: TContentType;
+    value: string;
+}
+
 interface IPerson {
     name: string;
     uri?: string;
@@ -13,7 +21,7 @@ interface ICategory {
 }
 
 interface IGenerator {
-    content: string;
+    value: string;
     uri?: string;
     version?: string;
 }
@@ -36,9 +44,9 @@ interface ISource {
     id: string;
     link?: ILink[];
     logo?: string;
-    rights?: string;
-    subtitle?: string;
-    title: string;
+    rights?: ITextConstruct;
+    subtitle?: ITextConstruct;
+    title: ITextConstruct;
     updated: string; // ISO
     // extensionElement: any;
 }
@@ -52,9 +60,9 @@ interface IAtomFeed {
     logo?: string;
     id: string;
     link?: ILink[];
-    rights?: string;
-    subtitle?: string;
-    title: string;
+    rights?: ITextConstruct;
+    subtitle?: ITextConstruct;
+    title: ITextConstruct;
     updated: string; // ISO
     // extensionElement: any;
 }
@@ -62,15 +70,15 @@ interface IAtomFeed {
 interface IAtomEntry {
     author: IPerson[];
     category?: ICategory[];
-    content: string;
+    content: TContent;
     contributor?: IPerson[];
     id: string;
     link?: ILink[];
     published?: string; // ISO
-    rights?: string;
+    rights?: ITextConstruct;
     atomSource?: ISource;
-    summary?: string;
-    title: string;
+    summary?: ITextConstruct;
+    title: ITextConstruct;
     updated: string; // ISO
     // extensionElement: any;
 }
@@ -86,9 +94,48 @@ interface IEntryData extends Omit<IAtomEntry, 'published' | 'updated'> {
     updated?: Date;
 }
 
-interface IAttribLink {
-    link: Array<{ _attributes: ILink }>;
+type TContentTypeField = {
+    _attributes: { type?: TContentType };
+    _text: string;
 }
+
+type IFeed = Readonly<{
+    author: IPerson[];
+    category?: ICategory[];
+    contributor?: IPerson[];
+    generator: {
+        _attributes: Omit<IGenerator, 'value'>;
+        _text: string;
+    };
+    icon?: string;
+    logo?: string;
+    id: string;
+    link: Array<{ _attributes: ILink }>;
+    rights?: TContentTypeField;
+    subtitle?: TContentTypeField;
+    title?: TContentTypeField;
+    updated: string;
+}>;
+type IEntry = {
+    author: IPerson[];
+    category?: ICategory[];
+    content: {
+        _attributes: {
+            type?: TContentType | string;
+            src?: string;
+        };
+        _text?: string;
+    };
+    contributor?: IPerson[];
+    id: string;
+    link: Array<{ _attributes: ILink }>;
+    published?: string;
+    rights?: TContentTypeField;
+    atomSource?: ISource;
+    summary?: TContentTypeField;
+    title: TContentTypeField;
+    updated: string;
+};
 
 const removeEmptyProperties = <T = IAtomFeed | IAtomEntry>(obj: T) => {
     const newObj: any = {};
@@ -101,8 +148,9 @@ const removeEmptyProperties = <T = IAtomFeed | IAtomEntry>(obj: T) => {
     return newObj as T;
 };
 
-type IFeed = Omit<IAtomFeed, 'link'> & IAttribLink;
-type IEntry = Omit<IAtomEntry, 'link'> & IAttribLink;
+const ifTextExist = <A>(_text: string | undefined, _attributes: A) => (
+    !_text ? undefined : { _attributes, _text }
+);
 
 export default class Feed {
     private readonly feed: IFeed;
@@ -114,17 +162,17 @@ export default class Feed {
             author,
             category,
             contributor,
-            generator = {
-                content: 'AtomFeed',
+            generator: { value: generatorText, ...generatorAttribs } = {
+                value: 'AtomFeed',
                 uri: 'https://github.com/thermarthae/atom-feed',
             },
             icon,
             logo,
             id,
             link = [],
-            rights,
-            subtitle,
-            title,
+            rights: { value: rightsText, ...rightsAttribs } = {} as Partial<ITextConstruct>,
+            subtitle: { value: subtitleText, ...subtitleAttribs } = {} as Partial<ITextConstruct>,
+            title: { value: titleText, ...titleAttribs } = {} as Partial<ITextConstruct>,
             updated = new Date(),
         } = feedData;
 
@@ -132,14 +180,17 @@ export default class Feed {
             author,
             category,
             contributor,
-            generator,
+            generator: {
+                _attributes: generatorAttribs,
+                _text: generatorText,
+            },
             icon,
             logo,
             id,
             link: link.map(_attributes => ({ _attributes })),
-            rights,
-            subtitle,
-            title,
+            rights: ifTextExist(rightsText, rightsAttribs),
+            subtitle: ifTextExist(subtitleText, subtitleAttribs),
+            title: ifTextExist(titleText, titleAttribs),
             updated: updated.toISOString(),
         };
 
@@ -150,30 +201,36 @@ export default class Feed {
         const {
             author,
             category,
-            content,
+            content: { value: contentText, ...contentAttribs } = {} as Partial<TContent>,
             contributor,
             id,
             link = [],
             published,
-            rights,
+            rights: { value: rightsText, ...rightsAttribs } = {} as Partial<ITextConstruct>,
             atomSource,
-            summary,
-            title,
+            summary: { value: summaryText, ...summaryAttribs } = {} as Partial<ITextConstruct>,
+            title: { value: titleText, ...titleAttribs },
             updated = new Date(),
         } = entryData;
 
         const entry: IEntry = removeEmptyProperties({
             author,
             category,
-            content,
+            content: {
+                _attributes: contentAttribs,
+                _text: contentText,
+            },
             contributor,
             id,
             link: link.map(_attributes => ({ _attributes })),
             published: published ? published.toISOString() : undefined,
-            rights,
+            rights: ifTextExist(rightsText, rightsAttribs),
             atomSource,
-            summary,
-            title,
+            summary: ifTextExist(summaryText, summaryAttribs),
+            title: {
+                _attributes: titleAttribs,
+                _text: titleText,
+            },
             updated: updated.toISOString(),
         });
 
